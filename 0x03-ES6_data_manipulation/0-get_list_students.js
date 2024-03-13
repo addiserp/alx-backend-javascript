c63754c00e4f12aeb7eec76ef1605df9d0e97480
Mikias Gedlu <addiserp@gmail.com>
2024-03-13 18:58:13 +0300

    0x03 data manuplationhot fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,8 +4,8 @@
  */
 export default function getListStudents() {
   return [
-    { id: 1, firstName: 'Mikias', location: 'Oromia' },
-    { id: 2, firstName: 'Eyob', location: 'Gambela' },
-    { id: 5, firstName: 'Abebe', location: 'Addis ababa' },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
