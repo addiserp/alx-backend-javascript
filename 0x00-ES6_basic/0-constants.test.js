917a065c7fae6a3ff850af00d7d39aeed5b17fc3
Mikias Gedlu <addiserp@gmail.com>
2024-03-13 18:50:47 +0300

    hot fix

--- 0x00-ES6_basic/0-constants.test.js
+++
@@ -1,5 +0,0 @@
-import { taskFirst, taskNext } from './0-constants.js';
-
-test('tasks are correctly defined', () => {
-  expect(`${taskFirst()} ${taskNext()}`).toBe('I prefer const when I can. But sometimes let is okay');
-});
